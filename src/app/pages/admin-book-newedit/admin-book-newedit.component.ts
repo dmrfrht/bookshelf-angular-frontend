1df95b3a72b66dbbd805b0c30b7d5944fe4adf56
dmrfrht <dmrfrht1@gmail.com>
2021-02-03 23:15:55 +0300

    kitap eklerken kategori ve resim icin bos olma durumlarinda hata gosterimleri duzeltildi

--- src/app/pages/admin-book-newedit/admin-book-newedit.component.ts
+++ src/app/pages/admin-book-newedit/admin-book-newedit.component.ts
@@ -31,6 +31,14 @@ export class AdminBookNeweditComponent implements OnInit {
   type: string;
   categories: Category[];
 
+  get category() {
+    return this.bookForm.get('categoryBy');
+  }
+
+  get pictureFile() {
+    return this.bookForm.get('pictureFile');
+  }
+
   upload(files) {
     const fileData = files.target.files[0];
     this.formData = new FormData();
@@ -69,7 +77,8 @@ export class AdminBookNeweditComponent implements OnInit {
       price: new FormControl('', Validators.required),
       stock: new FormControl('', Validators.required),
       picture: new FormControl(''),
-      categoryBy: new FormControl('')
+      categoryBy: new FormControl('', Validators.required),
+      pictureFile: new FormControl('', Validators.required)
     });
   }
 
