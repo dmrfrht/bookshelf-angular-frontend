4446f0e1c7b7db94ca9e3dd813627130e9540db0
dmrfrht <dmrfrht1@gmail.com>
2021-02-04 23:47:38 +0300

    uppdate

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/book-app-heroku'));
+app.use(express.static('./dist/bookshelf-angular-frontend'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/book-app-heroku/' }),
+  res.sendFile('index.html', {root: 'dist/bookshelf-angular-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
